fab4f3fe8d74f5284fa529b7aa073ecb0d010e8b
YajatKaul <mc.crafter1973@gmail.com>
2024-06-20 23:35:37 +0530

    modified:   tauri-app/src-tauri/Cargo.lock 	modified:   tauri-app/src-tauri/Cargo.toml 	modified:   tauri-app/src-tauri/src/main.rs 	modified:   tauri-app/src-tauri/tauri.conf.json 	modified:   tauri-app/src/App.tsx

--- tauri-app/src-tauri/src/main.rs
+++ tauri-app/src-tauri/src/main.rs
@@ -1,15 +1,37 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+#![cfg_attr(
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
+)]
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![list_drives])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
+
+#[tauri::command]
+fn list_drives() -> Vec<String> {
+    use std::fs;
+
+    let mut drives = Vec::new();
+
+    match fs::read_dir("C:\\") {
+        Ok(entries) => {
+            for entry in entries {
+                match entry {
+                    Ok(entry) => {
+                        if let Some(path_str) = entry.path().to_str() {
+                            drives.push(path_str.to_string());
+                        }
+                    },
+                    Err(e) => eprintln!("Error: {}", e),
+                }
+            }
+            println!("{:?}",drives);
+        }
+        Err(e) => eprintln!("Error: {}", e),
+    }
+    return drives
+}
+
