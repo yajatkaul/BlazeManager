49b2edb8662bfd154bd8f63be35ccd2294074574
YajatKaul <mc.crafter1973@gmail.com>
2024-06-21 00:56:19 +0530

    modified:   tauri-app/src-tauri/src/main.rs 	modified:   tauri-app/src/App.tsx

--- tauri-app/src-tauri/src/main.rs
+++ tauri-app/src-tauri/src/main.rs
@@ -5,7 +5,7 @@
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![list_drives])
+        .invoke_handler(tauri::generate_handler![list_drives,list_dir])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
@@ -17,6 +17,30 @@ fn list_drives() -> Vec<String> {
     let mut drives = Vec::new();
 
     match fs::read_dir("C:\\") {
+        Ok(entries) => {
+            for entry in entries {
+                match entry {
+                    Ok(entry) => {
+                        if let Some(path_str) = entry.path().to_str() {
+                            drives.push(path_str.to_string());
+                        }
+                    },
+                    Err(e) => eprintln!("Error: {}", e),
+                }
+            }
+        }
+        Err(e) => eprintln!("Error: {}", e),
+    }
+    return drives
+}
+
+#[tauri::command]
+fn list_dir(path: String) -> Vec<String> {
+    use std::fs;
+
+    let mut drives = Vec::new();
+
+    match fs::read_dir(&path) {
         Ok(entries) => {
             for entry in entries {
                 match entry {
--- tauri-app/src/App.tsx
+++ tauri-app/src/App.tsx
@@ -8,6 +8,7 @@ interface Drive {
 
 const App: React.FC = () => {
   const [drives, setDrives] = useState<Drive[]>([]);
+  const [files, setFiles] = useState<Drive[]>([]);
 
   useEffect(() => {
     fetchDrives();
@@ -15,8 +16,18 @@ const App: React.FC = () => {
 
   const fetchDrives = async () => {
     try {
-      const driveList = await invoke<Drive[]>("list_drives");
+      const driveList = await invoke<Drive[]>(`list_drives`);
       setDrives(driveList);
+    } catch (error) {
+      console.error("Error fetching drives:", error);
+    }
+  };
+
+  const loadFiles = async (path) => {
+    try {
+      const driveList = await invoke<Drive[]>("list_dir", path);
+      console.log(driveList);
+      setFiles(driveList);
     } catch (error) {
       console.error("Error fetching drives:", error);
     }
@@ -27,11 +38,19 @@ const App: React.FC = () => {
       <h1>Available Drives</h1>
       <ul>
         {drives.map((drive, index) => (
-          <li key={index} style={{ cursor: "pointer" }}>
+          <li
+            key={index}
+            style={{ cursor: "pointer" }}
+            onClick={() => loadFiles(drive)}
+          >
             {drive}
           </li>
         ))}
       </ul>
+      <p>Files</p>
+      {files.map((path) => (
+        <li>{path}</li>
+      ))}
     </div>
   );
 };
